db97b500676926e4661c61427a86a684962479ce
Félix Piédallu <felix@piedallu.me>
2016-11-17 00:20:49 +0100

    cleaner libopencm3 + gitignore

--- Code/nano-robot/libopencm3.cmake
+++ Code/nano-robot/libopencm3.cmake
@@ -17,7 +17,7 @@ list(APPEND toolchain_libs m c gcc nosys)
 if(${BOARD} STREQUAL "stm32f0")
     set(BOARD_CFlags
         "-mcpu=cortex-m0 -mthumb \
-        -DSTM32F0 -Tlibopencm3_stm32f0.ld")
+        -DSTM32F0 -Tstm32f0.ld")
     list(APPEND toolchain_libs opencm3_stm32f0)
     set(OpenOCD_CFG "/usr/share/openocd/scripts/board/st_nucleo_f0.cfg")
 
@@ -49,13 +49,12 @@ elseif(${BOARD} STREQUAL "stm32l4")
 
 endif()
 
-set(CMAKE_C_FLAGS "-DDEBUG=${DEBUG} -L${TOOLCHAIN_PATH} ${BOARD_CFlags} ${stm32_common_CFlags}"
+set(CMAKE_C_FLAGS "-DDEBUG=${DEBUG} -L${TOOLCHAIN_PATH} -L${TOOLCHAIN_PATH}/libopencm3/lib ${BOARD_CFlags} ${stm32_common_CFlags}"
 CACHE STRING "" FORCE)
 
 set(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
 
 if (NOT TARGET flash)
-
 
 add_custom_target(flash
     DEPENDS main_binary
@@ -75,29 +74,18 @@ if (NOT TARGET lib-stm32f4)
 
 add_custom_target(lib-stm32f0 WORKING_DIRECTORY ${TOOLCHAIN_PATH}
     COMMAND make -C libopencm3 lib/stm32/f0
-    COMMAND cp libopencm3/lib/libopencm3_stm32f0.a  .
-    COMMAND cp libopencm3/lib/libopencm3_stm32f0.ld .
 )
 add_custom_target(lib-stm32f1 WORKING_DIRECTORY ${TOOLCHAIN_PATH}
     COMMAND make -C libopencm3 lib/stm32/f1
-    COMMAND cp libopencm3/lib/libopencm3_stm32f1.a  .
-    COMMAND cp libopencm3/lib/libopencm3_stm32f1.ld .
 )
 add_custom_target(lib-stm32f3 WORKING_DIRECTORY ${TOOLCHAIN_PATH}
     COMMAND make -C libopencm3 lib/stm32/f3
-    COMMAND cp libopencm3/lib/libopencm3_stm32f3.a  .
-    COMMAND cp libopencm3/lib/libopencm3_stm32f3.ld .
 )
 add_custom_target(lib-stm32f4 WORKING_DIRECTORY ${TOOLCHAIN_PATH}
     COMMAND make -C libopencm3 lib/stm32/f4
-    COMMAND cp libopencm3/lib/libopencm3_stm32f4.a  .
-    COMMAND cp libopencm3/lib/libopencm3_stm32f4.ld .
 )
-
 add_custom_target(lib-stm32l4 WORKING_DIRECTORY ${TOOLCHAIN_PATH}
     COMMAND make -C libopencm3 lib/stm32/l4
-    COMMAND cp libopencm3/lib/libopencm3_stm32l4.a  .
-    COMMAND cp libopencm3/lib/libopencm3_stm32l4.ld .
 )
 
 endif()
