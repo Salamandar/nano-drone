78830e4915a2a25b34e2109c09a4d63a887c5bb0
Félix Piédallu <felix@piedallu.me>
2016-11-23 14:45:21 +0100

    added debug script

---
+++ Code/nano-robot/debug.sh
@@ -0,0 +1,11 @@
+openocd \
+    -f interface/stlink-v2-1.cfg    \
+    -f target/stm32f0x.cfg          \
+    -c "telnet_port 4444"           \
+    -c "gdb_port    3333"           \
+    -c "init" -c "reset init"       \
+    >/dev/null &
+
+telnet localhost 4444 >/dev/null &
+
+arm-none-eabi-gdb --eval-command="target remote localhost:3333" NanoRobot
